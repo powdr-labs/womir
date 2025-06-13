ee30ff5edbbc5c2b9d212769a74e26b70c0336ba
Leo Alt <leo@powdrlabs.com>
2025-06-13 12:03:41 +0200

    add another keccak example

---
+++ sample-programs/keccak_with_inputs/src/main.rs
@@ -0,0 +1,18 @@
+#[link(wasm_import_module = "env")]
+unsafe extern "C" {
+    pub safe fn read_u32(idx: u32) -> u32;
+    pub safe fn abort();
+}
+
+use tiny_keccak::{Hasher, Keccak};
+
+pub fn main() {
+    let n: u32 = read_u32(0);
+    let mut output = [0u8; 32];
+    for _ in 0..n {
+        let mut hasher = Keccak::v256();
+        hasher.update(&output);
+        hasher.finalize(&mut output);
+    }
+    assert_eq!(output[0] as u32, 0x29);
+}
