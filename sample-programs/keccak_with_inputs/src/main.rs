c986c0110d27921cdf0ae2f04a8f5aa398a9bff2
Leo Alt <leo@powdrlabs.com>
2025-06-14 17:04:29 +0200

    run end to end rust to interpreter tests

--- sample-programs/keccak_with_inputs/src/main.rs
+++ sample-programs/keccak_with_inputs/src/main.rs
@@ -8,11 +8,14 @@ use tiny_keccak::{Hasher, Keccak};
 
 pub fn main() {
     let n: u32 = read_u32(0);
+    let expected_first_byte: u32 = read_u32(1);
+
     let mut output = [0u8; 32];
     for _ in 0..n {
         let mut hasher = Keccak::v256();
         hasher.update(&output);
         hasher.finalize(&mut output);
     }
-    assert_eq!(output[0] as u32, 0x29);
+
+    assert_eq!(output[0] as u32, expected_first_byte);
 }
